fc4f28981ed1e1613d922d298c0e698b1f81963d
Jedrzej <gruszkojados@gmail.com>
2020-12-17 09:43:25 +0100

    nav

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { MeComponent } from './pages/me/me.component';
+import { ExpComponent } from './pages/exp/exp.component';
+import { SkillComponent } from './pages/skill/skill.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    NavbarComponent,
+    MeComponent,
+    ExpComponent,
+    SkillComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
